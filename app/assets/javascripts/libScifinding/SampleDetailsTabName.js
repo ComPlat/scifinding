39ac997abe591aa6658eb7db19695885295cdea9
PiTrem <pierre.tremouilhac@gmx.de>
2017-02-28 16:56:11 +0100

    change sf logo change style reaction sample selection button fix typo

--- app/assets/javascripts/libScifinding/SampleDetailsTabName.js
+++ app/assets/javascripts/libScifinding/SampleDetailsTabName.js
@@ -1,3 +1,3 @@
 import React, {Component} from 'react';
 
-export default  <img src="images/signInLogo.png" alt="Scifinding" style={{"maxHeight":25}}></img>
+export default  <img src="images/sf_logo_text.png" alt="Scifinding" style={{"maxHeight":25}}></img>
